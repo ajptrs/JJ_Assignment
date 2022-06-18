ce2d55eb6a7232f5fe35aaa67c9b46e22539c00e
Alex Petras <alex.petras.23@gmail.com>
2022-06-17 21:45:03 -0700

    Huge Commit, we're on the cusp of something B.I.G.

--- JJ_matrix.cpp
+++ JJ_matrix.cpp
@@ -1,17 +1,51 @@
 #include <iostream>
-int main() {
-    int test[2][2];
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            test[i][j] = 0;
-        }
-    }
+#include <vector>
 
-    test[1][2] = 1;
-    std::cout << test << std::endl;
-    //switcharoo();
+using namespace std;
+
+void func(vector< vector<int> > &myMatrix) { //pass by reference to not copy entire matrix 
+
 }
 
-void switcharoo() {
-    int hello;
+void myPrintFunc(vector< vector<int> > &myMatrix) { 
+    for (int m = 0; m < 3; m++){
+        cout << "Row " << m << ": " << endl;
+        for (int i = 0; i < 5; i++) {
+            int printout = myMatrix[m][i];
+            cout << printout << ", ";
+        }
+        cout << endl;
+    }
+}
+
+
+int main() {
+    vector< vector<int> > myMatrix;
+    vector<int> myRow;
+    for (int m = 0; m < 3; m++){
+        myRow.clear();
+        for (int i = 0; i < 5; i++) {
+            myRow.push_back(0);
+        }
+        myMatrix.push_back(myRow);
+    }
+    int sizeM = myMatrix.size(); //this gives the number of rows 
+    int sizeM1 = myMatrix[0].size(); // this gives the number columns
+
+
+
+    myMatrix[2][4]=23; //test this one 
+    cout << "Before function: " <<  endl;
+    myPrintFunc(myMatrix);
+
+    func(myMatrix);
+    cout << "After Function: " <<  endl;
+    myPrintFunc(myMatrix);   
+
+
+    /*
+    int temp1;
+    temp1 = myMatrix[0][1];
+    cout << "myMatrix[0][1] after going into fucntion: " << temp1 << endl;
+    */
 }
