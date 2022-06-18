40edcbd94ef46d9210cce52803d27b6ed65bb26e
Alex Petras <alex.petras.23@gmail.com>
2022-06-17 18:06:51 -0700

    (U) More first changes

--- JJ_matrix.cpp
+++ JJ_matrix.cpp
@@ -1,5 +1,14 @@
 int main() {
-    switcharoo();
+    int test[2][2];
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            test[i][j] = 0;
+        }
+    }
+
+    test[1][2] = 1;
+    std::cout << test << std::endl;
+    //switcharoo();
 }
 
 void switcharoo() {
